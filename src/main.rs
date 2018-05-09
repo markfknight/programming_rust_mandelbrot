66c7030247c5f5f01042abbdbab68d15fff04c5d
Mark Knight <mark@markfknight.com>
2018-05-09 21:59:35 +0100

    feat: parsing pair command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate num;
 use num::Complex;
+use std::str::FromStr;
 
 fn main() {
     println!("Hello, world!");
@@ -45,4 +46,35 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
         }
     }
     None
+}
+
+/// Parse the string 's' as a coordinate pair, like '"400x600"' or '"1.0.0.5"'.
+///
+/// Specifically 's' should have the form <left><sep><right>, where <sep> is
+/// the character given by the 'separator' argument, and <left> and <right> are
+/// both strings that can  be parsed by 'T::from_str'.
+///
+/// If 's' has the proper form, return 'Some<(x, y)>'. If it doesn't parse
+/// correctly, return 'None'.
+fn parse_pair<T: FromStr>(s: &str, seperator: char) -> Option<(T, T)> {
+    match s.find(seperator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
