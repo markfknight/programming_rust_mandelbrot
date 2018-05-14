d2031df6d64f32fd143a0ebf8b14fe2f31dd519d
Mark Knight <mark@markfknight.com>
2018-05-14 21:13:55 +0100

    feat: plotting the set

--- src/main.rs
+++ src/main.rs
@@ -97,6 +97,27 @@ fn pixel_to_point(bounds: (usize, usize), pixel: (usize, usize), upper_left: Com
     }
 }
 
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+///
+/// The `bounds` argument gives the width and height of the buffer `pixels`,
+/// which holds one greyscale pixel per byte. The `upper_left` and `lower_right`
+/// arguments specify points on the complex place corresponding to the upper-
+/// left and lower-right corners of the pixel buffer.
+fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0 .. bounds.1 {
+        for column in 0 .. bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] =
+                match escape_time(point, 255) {
+                    None => 0,
+                    Some(count) => 255 * count as u8
+                };
+        }
+    }
+}
+
 #[test]
 fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("", ','), None);
