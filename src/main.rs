29b72edfae4e9004b6fcbe62e7f7bf00a07bc13f
Mark Knight <mark@markfknight.com>
2018-05-15 21:16:00 +0100

    feat: single thread main

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,25 @@ use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
 use std::io::Result;
+use std::io::Write;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        writeln!(std::io::stderr(), "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT").unwrap();
+        writeln!(std::io::stderr(), "Example: {} mandel.png 1000x750, -1.20,0.35 -1.0,0.20", args[0]).unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right coner point");
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
 #[allow(dead_code)]
@@ -118,7 +134,7 @@ fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, l
             pixels[row * bounds.0 + column] =
                 match escape_time(point, 255) {
                     None => 0,
-                    Some(count) => 255 * count as u8
+                    Some(count) => 255 - count as u8
                 };
         }
     }
